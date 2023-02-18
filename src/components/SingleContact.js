d73da0989acf04c12207ed196ba5d86d2e28cf56
Himanshu Hota <Himanshuhota55@gmail.com>
2023-02-18 10:43:08 +0530

    minor fix

--- src/components/SingleContact.js
+++ src/components/SingleContact.js
@@ -23,39 +23,39 @@ const SingleContact = () => {
 
             {id &&
 
-                <div className="single w-[100%] md:w-[70%] h-[80%] bg-gray-300 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-30 border border-gray-100 p-5 flex flex-col justify-start items-center">
+                <div className="single w-[100%] md:w-[70%] h-[85%] bg-gray-300 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-30 border border-gray-100 p-5 flex flex-col justify-start items-center">
                     <img src="https://w7.pngwing.com/pngs/831/88/png-transparent-user-profile-computer-icons-user-interface-mystique-miscellaneous-user-interface-design-smile-thumbnail.png" alt="person" className='w-16 h-auto rounded-full mb-3' />
 
-                    <div className='w-full py-2 text-xl'>
+                    <div className='w-full py-2 text-[1rem]'>
                         <h2>Name : {name} </h2>
                     </div>
-                    <div className='w-full py-2 text-xl'>
+                    <div className='w-full py-2 text-[1rem]'>
                         <p>Email : {email} </p>
                     </div>
-                    <div className='w-full py-2 text-xl'>
+                    <div className='w-full py-2 text-[1rem]'>
                         <p>Phone : {phone} </p>
                     </div>
-                    <div className='w-full py-2 text-xl'>
+                    <div className='w-full py-2 text-[1rem]'>
                         <p>Username : @{username} </p>
                     </div>
-                    <div className='w-full py-2 text-xl'>
+                    <div className='w-full py-2 text-[1rem]'>
                         <p>Website : {website} </p>
                     </div>
-                    <div className=' w-full py-2 text-xl flex justify-between  items-start'>
+                    <div className=' w-full py-2 text-[1rem] flex justify-between  items-start'>
                         <p>Address : </p>
 
                         <div className="address w-[70%] text-base">
-                            <div className="street flex justify-start">
+                            <div className="street flex justify-start text-[1rem]">
                                 <p>Street : </p>
                                 <p className="px-3">{address.street}</p>
                             </div>
 
-                            <div className="City flex justify-start">
+                            <div className="City flex justify-start text-[1rem]">
                                 <p>City : </p>
                                 <p className="px-3">{address.city}</p>
                             </div>
 
-                            <div className="Zip flex justify-start">
+                            <div className="Zip flex justify-start text-[1rem]">
                                 <p>Zip : </p>
                                 <p className="px-3">{address.zipcode}</p>
                             </div>
